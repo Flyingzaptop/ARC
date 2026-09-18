1145f510ffbaea78ae8a6e6194a86541d56da810
Flyingzaptop <47990193+Flyingzaptop@users.noreply.github.com>
2026-09-18 10:30:08 +0200

    Add Wicked harness bridge surface

---
+++ integrations/wicked-engine/ArcWickedBridge.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "stdafx.h"
+
+namespace arc_wicked {
+
+void HarnessUpdate(
+    wi::gui::ComboBox& test_selector,
+    std::uint32_t width,
+    std::uint32_t height) noexcept;
+
+void ForceFullQuality() noexcept;
+bool Finished() noexcept;
+
+} // namespace arc_wicked
