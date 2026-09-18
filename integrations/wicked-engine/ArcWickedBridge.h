ffe8953d3721d057a30944eb4539af883aac60c5
Flyingzaptop <47990193+Flyingzaptop@users.noreply.github.com>
2026-09-18 11:31:27 +0200

    Pass Wicked render path to ARC harness

--- integrations/wicked-engine/ArcWickedBridge.h
+++ integrations/wicked-engine/ArcWickedBridge.h
@@ -5,6 +5,7 @@
 namespace arc_wicked {
 
 void HarnessUpdate(
+    wi::RenderPath3D& render_path,
     wi::gui::ComboBox& test_selector,
     std::uint32_t width,
     std::uint32_t height) noexcept;
