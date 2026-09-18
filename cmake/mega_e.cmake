ea90d1d34c9f318611bf5118540a7aa5fbd2c49a
Flyingzaptop <47990193+Flyingzaptop@users.noreply.github.com>
2026-09-18 22:18:46 +0200

    Wire Mega E core tests and visual debugger

---
+++ cmake/mega_e.cmake
@@ -0,0 +1,28 @@
+target_sources(arc-core PRIVATE
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/core/temporal_visibility.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/core/visual_importance.cpp
+)
+
+if(ARC_BUILD_TESTS)
+    add_executable(arc-temporal-visibility-tests
+        ${CMAKE_CURRENT_SOURCE_DIR}/tests/temporal_visibility_tests.cpp)
+    target_link_libraries(arc-temporal-visibility-tests PRIVATE arc::core)
+    add_test(NAME arc-temporal-visibility-tests COMMAND arc-temporal-visibility-tests)
+
+    add_executable(arc-visual-importance-tests
+        ${CMAKE_CURRENT_SOURCE_DIR}/tests/visual_importance_tests.cpp)
+    target_link_libraries(arc-visual-importance-tests PRIVATE arc::core)
+    add_test(NAME arc-visual-importance-tests COMMAND arc-visual-importance-tests)
+
+    add_executable(arc-mega-e-scenario-tests
+        ${CMAKE_CURRENT_SOURCE_DIR}/tests/mega_e_scenario_tests.cpp)
+    target_link_libraries(arc-mega-e-scenario-tests PRIVATE arc::core)
+    add_test(NAME arc-mega-e-scenario-tests COMMAND arc-mega-e-scenario-tests)
+endif()
+
+if(WIN32)
+    add_executable(arc-mega-e-visual-fps WIN32
+        ${CMAKE_CURRENT_SOURCE_DIR}/samples/mega_e_visual_fps.cpp)
+    target_link_libraries(arc-mega-e-visual-fps PRIVATE arc::core user32 gdi32)
+    target_compile_features(arc-mega-e-visual-fps PRIVATE cxx_std_23)
+endif()
